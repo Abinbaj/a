254c27de9462ce3503e8cb317b05d2cd0bd73cb5
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-04 02:40:57 +1000

    update response item data class to fix error

--- app/src/main/java/com/vu/networkexample/homescreen/data/ResponseItem.kt
+++ app/src/main/java/com/vu/networkexample/homescreen/data/ResponseItem.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.Json
 data class ResponseItem(
     @Json(name = "id") val id: String,
     @Json(name = "name") val name: String,
-    @Json(name = "data") val data: Map<String, String>
+    @Json(name = "data") val data: Map<String, String>?
 )
