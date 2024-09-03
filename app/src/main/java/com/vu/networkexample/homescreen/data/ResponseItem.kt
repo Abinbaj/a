c97c782e6c57b227a6a9157a6fa7ec43dd3ea1da
MDeMel-Dev <manendra.demel@beemit.com.au>
2024-09-04 01:43:42 +1000

    Create data class to match json response

---
+++ app/src/main/java/com/vu/networkexample/homescreen/data/ResponseItem.kt
@@ -0,0 +1,9 @@
+package com.vu.networkexample.homescreen.data
+
+import com.squareup.moshi.Json
+
+data class ResponseItem(
+    @Json(name = "id") val id: String,
+    @Json(name = "name") val name: String,
+    @Json(name = "data") val data: Map<String, String>
+)
